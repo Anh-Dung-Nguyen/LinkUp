f5618fe97250063a004b7b0390a67971016a5824
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-11-06 14:11:04 +0100

    Add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,12 +5,17 @@ import userRoutes from "./routes/userRoutes.js";
 import chatRoutes from "./routes/chatRoutes.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
