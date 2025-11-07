87a8c203489376e02a6072f759ffa9f631177121
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-11-07 11:51:28 +0100

    Add authUser for tanstack query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router';
+import { Navigate, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import SignupPage from './pages/SignupPage';
 import LoginPage from './pages/LoginPage';
@@ -8,18 +8,31 @@ import CallPage from './pages/CallPage';
 import OnboardingPage from './pages/OnboardingPage';
 import NotificationPage from './pages/NotificationPage';
 import { Toaster } from 'react-hot-toast';
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios.js";
 
 const App = () => {
+  const {data:authData, isLoading, error} = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false,
+  });
+
+  const authUser = authData?.user;
+
   return (
     <div className = 'h-screen' data-theme='night'>
       <Routes>
-        <Route path = '/' element = {<HomePage />} />
-        <Route path = '/signup' element = {<SignupPage />} />
-        <Route path = '/login' element = {<LoginPage />} />
-        <Route path = '/chat' element = {<ChatPage />} />
-        <Route path = '/call' element = {<CallPage />} />
-        <Route path = '/onboarding' element = {<OnboardingPage />} />
-        <Route path = '/notification' element = {<NotificationPage />} />
+        <Route path = '/' element = {authUser ? <HomePage /> : <Navigate to = "/login" />} />
+        <Route path = '/signup' element = {!authUser ? <SignupPage /> : <Navigate to = "/" />} />
+        <Route path = '/login' element = {!authUser ? <LoginPage /> : <Navigate to = "/" />} />
+        <Route path = '/chat' element = {authUser ? <ChatPage /> : <Navigate to = "/login" />} />
+        <Route path = '/call' element = {authUser ? <CallPage /> : <Navigate to = "/login" />} />
+        <Route path = '/onboarding' element = {authUser ? <OnboardingPage /> : <Navigate to = "/login" />} />
+        <Route path = '/notification' element = {authUser ? <NotificationPage /> : <Navigate to = "/login" />} />
       </Routes>
 
       <Toaster/>
