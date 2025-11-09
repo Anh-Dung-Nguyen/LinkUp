5195894de90c23234a7717acbe1a99d5fc05c100
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-11-09 17:43:20 +0100

    Update route logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,8 +28,8 @@ const App = () => {
           ? (<HomePage />) 
           : (<Navigate to = {!isAuthenticated ? "/login" : "/onboarding"} />)} 
         />
-        <Route path = '/signup' element = {!isAuthenticated ? <SignupPage /> : <Navigate to = "/" />} />
-        <Route path = '/login' element = {!isAuthenticated ? <LoginPage /> : <Navigate to = "/" />} />
+        <Route path = '/signup' element = {!isAuthenticated ? <SignupPage /> : <Navigate to = {isOnboarded ? "/" : "/onboarding"} />} />
+        <Route path = '/login' element = {!isAuthenticated ? <LoginPage /> : <Navigate to = {isOnboarded ? "/" : "/onboarding"} />} />
         <Route path = '/chat' element = {isAuthenticated ? <ChatPage /> : <Navigate to = "/login" />} />
         <Route path = '/call' element = {isAuthenticated ? <CallPage /> : <Navigate to = "/login" />} />
         <Route path = '/onboarding' element = {isAuthenticated ? (
