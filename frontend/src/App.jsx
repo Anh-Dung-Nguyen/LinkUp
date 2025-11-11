081b5a7cf713b14e85a9aa7628496f000d5c0829
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-11-11 19:07:51 +0100

    Add theme selector to all web

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,10 +11,11 @@ import { Toaster } from 'react-hot-toast';
 import PageLoader from './components/PageLoader.jsx';
 import useAuthUser from './hooks/useAuthUser.js';
 import Layout from './components/Layout.jsx';
+import { useThemeStore } from './store/useThemeStore.js';
 
 const App = () => {
   const {isLoading, authUser} = useAuthUser();
-
+  const {theme} = useThemeStore();
   const isAuthenticated = Boolean(authUser);
   const isOnboarded = authUser?.isOnboarded;
 
@@ -23,7 +24,7 @@ const App = () => {
   }
 
   return (
-    <div className = 'h-screen' data-theme='night'>
+    <div className = 'h-screen' data-theme = {theme}>
       <Routes>
         <Route path = '/' element = {isAuthenticated && isOnboarded 
           ? (
